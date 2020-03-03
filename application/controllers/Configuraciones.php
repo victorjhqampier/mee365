c9fc8525ebf20dab180d6f60fca89845c29e1b11
root <you@example.com>
2020-03-03 18:49:37 -0500

    empledos

--- application/controllers/Configuraciones.php
+++ application/controllers/Configuraciones.php
@@ -4,7 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Configuraciones extends CI_Controller {
 	function __construct(){
 		parent::__construct();
-		$controlador=explode("/",$_SERVER['PHP_SELF']);$this->load->library('serv_administracion_usuarios');if(!$this->serv_administracion_usuarios->cargar_app_session($controlador[3])){show_404();}
+		$controlador=explode("/",$_SERVER['PHP_SELF']);
+		$this->load->library('serv_administracion_usuarios');
+		if(!$this->serv_administracion_usuarios->cargar_app_session($controlador[3])){show_404();}//comprueba la sesion
 	}
 	public function index(){
 		$this->load->view('arixshellbase');
@@ -16,6 +18,21 @@ class Configuraciones extends CI_Controller {
 		else{
 			show_404();
 		}
-		
+	}
+	public function empleados(){
+		if ($this->input->is_ajax_request()) {
+			$this->load->view('app_configuraciones/sucursales');
+		}
+		else{
+			show_404();
+		}
+	}
+	public function areas(){
+		if ($this->input->is_ajax_request()) {
+			$this->load->view('app_configuraciones/sucursales');
+		}
+		else{
+			show_404();
+		}
 	}
 }
